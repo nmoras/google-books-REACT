dd34568a0c06426b8b9b2ee49df661c96e8c6731
poem <normamoras@gmail.com>
2020-04-09 17:09:07 -0400

    updated api call

--- src/components/BookData.js
+++ src/components/BookData.js
@@ -1,11 +1,58 @@
 import React from 'react'
 
-function BookData() {
+
+
+function BookData(props) {
+    console.log(props)
+    // let book = props.items.volumeInfo;
+    // let image = props.items.volumeInfo.imageLinks.thumbnail;
+        
+
+    
     return (
         <div>
-            <h1>book Details</h1>
+            {props.bookList.map( item => (
+                    <div class='row justify-content-center'>
+                         {/* <div class='col-11 p-2' style={employeeListStyle.colborder}> */}
+                            <div class="row">
+                                <div class="col-3 imgcontainer">
+                                    <img class="img-thumbnail" alt={item.book.name} src={item.image} />
+                                </div>
+                                <div class="col-9 emp-content">
+                                    <div class='row'>
+                                        <div class='col'>
+                                            <ul >
+                                                <li >
+                                                    <strong>Title:</strong> {item.book.name}
+                                                </li>
+                                                <li>
+                                                    <strong>Author:</strong> {item.book.author}
+                                                </li>
+                                                <li>
+                                                    <strong>Average Rating:</strong> {item.book.averageRating}
+                                                </li>
+                                                <li>
+                                                    <strong>Description:</strong> {item.book.description}
+                                                </li>
+                                                <li>
+                                                    <strong>Infolink:</strong> {item.book.infoLink}
+                                                </li>
+                                            </ul>
+                                    </div>
+                               </div>
+                               
+                           </div>
+                       </div>
+                     
+                    </div>
+               
+
+             ) )}
             
-        </div>
+
+        </div> 
+            
+    
     )
 }
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -17,11 +17,19 @@ function SearchBar(props) {
     async function handleClick(e){
         e.preventDefault();
         let bookListResult = await fetch( `https://www.googleapis.com/books/v1/volumes?q=${myInput}` ).then( result => result.json()  )
-         let sortBookListResult = [...bookListResult]
         
-        setBookList( sortBookListResult )
+        setBookList( bookListResult.items )
+        // console.log(bookList);
 
-        console.log( bookListResult )
+
+        // console.log( bookListResult );
+        // console.log(bookListResult.items[0].volumeInfo)
+        // console.log(bookListResult.items[0].volumeInfo.title)
+        // console.log(bookListResult.items[0].volumeInfo.authors)
+        // console.log(bookListResult.items[0].volumeInfo.description)
+        // console.log(bookListResult.items[0].volumeInfo.averageRating)
+        // console.log(bookListResult.items[0].volumeInfo.imageLinks.thumbnail)
+        // console.log(bookListResult.items[0].volumeInfo.infoLink)
         setMyInput(' ');
 
     }
