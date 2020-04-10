7d6070ecdcb9552eff6a09dba100afc628c12410
poem <normamoras@gmail.com>
2020-04-09 22:21:18 -0400

    booklist display working

--- src/components/BookData.js
+++ src/components/BookData.js
@@ -3,57 +3,62 @@ import React from 'react'
 
 
 function BookData(props) {
-    console.log(props)
-    // let book = props.items.volumeInfo;
-    // let image = props.items.volumeInfo.imageLinks.thumbnail;
-        
-
+    console.log('the props is', props)
+    // console.log(props.bookList[0].volumeInfo.title);
+    const Style = {
+        listtype: {listStyleType: 'none'},
+    }
+   function handleClick(){
+       console.log('i am saved')
+   }
     
     return (
         <div>
             {props.bookList.map( item => (
-                    <div class='row justify-content-center'>
-                         {/* <div class='col-11 p-2' style={employeeListStyle.colborder}> */}
-                            <div class="row">
+                    <div class='row justify-content-center mt-2' style={{backgroundColor: "white"}}>
+                         <div class='col-11 p-2' >
+                             <div class="row">
                                 <div class="col-3 imgcontainer">
-                                    <img class="img-thumbnail" alt={item.book.name} src={item.image} />
+                                    <img class="img-thumbnail" alt={item.volumeInfo.title} src={item.volumeInfo.imageLinks.thumbnail} />
                                 </div>
                                 <div class="col-9 emp-content">
                                     <div class='row'>
                                         <div class='col'>
-                                            <ul >
+                                            <ul style={Style.listtype} >
                                                 <li >
-                                                    <strong>Title:</strong> {item.book.name}
+                                                    <strong>Title:</strong> {item.volumeInfo.title}
                                                 </li>
                                                 <li>
-                                                    <strong>Author:</strong> {item.book.author}
+                                                    <strong>Author:</strong> {item.volumeInfo.authors}
                                                 </li>
                                                 <li>
-                                                    <strong>Average Rating:</strong> {item.book.averageRating}
+                                                    <strong>Average Rating:</strong> {item.volumeInfo.averageRating}
                                                 </li>
                                                 <li>
-                                                    <strong>Description:</strong> {item.book.description}
+                                                    <strong>Description:</strong> {item.volumeInfo.description}
                                                 </li>
                                                 <li>
-                                                    <strong>Infolink:</strong> {item.book.infoLink}
+                                                    <strong>Infolink:</strong> {item.volumeInfo.infoLink}
                                                 </li>
                                             </ul>
+                                        </div>
+                                        <div>
+                                            <button style={{bacgroundColor: 'black', padding: '5px', color: 'white'}}>View</button>
+                                            <button onClick={handleClick}>Save</button>
+                                        
+                                        </div>
                                     </div>
-                               </div>
                                
-                           </div>
-                       </div>
+                                    </div>
+                                </div>
                      
-                    </div>
-               
-
-             ) )}
+                            </div>
+                        </div>    
+                 ) )}       
             
 
         </div> 
-            
-    
+
     )
 }
-
 export default BookData
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -14,22 +14,12 @@ function SearchBar(props) {
         console.log(myInput)
     }
 
-    async function handleClick(e){
+   async function handleClick(e){
         e.preventDefault();
         let bookListResult = await fetch( `https://www.googleapis.com/books/v1/volumes?q=${myInput}` ).then( result => result.json()  )
         
         setBookList( bookListResult.items )
         // console.log(bookList);
-
-
-        // console.log( bookListResult );
-        // console.log(bookListResult.items[0].volumeInfo)
-        // console.log(bookListResult.items[0].volumeInfo.title)
-        // console.log(bookListResult.items[0].volumeInfo.authors)
-        // console.log(bookListResult.items[0].volumeInfo.description)
-        // console.log(bookListResult.items[0].volumeInfo.averageRating)
-        // console.log(bookListResult.items[0].volumeInfo.imageLinks.thumbnail)
-        // console.log(bookListResult.items[0].volumeInfo.infoLink)
         setMyInput(' ');
 
     }
@@ -46,8 +36,8 @@ function SearchBar(props) {
                             </div>
                         </div>
             </div>
-            <BookData bookList ={bookList} />
-        </div>
+            <BookData bookList={bookList}/>
+    </div>    
     )
 }
 
