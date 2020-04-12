f35d8ae2de7ec7a918b1cb707ae90608eeb6bd2c
poem <normamoras@gmail.com>
2020-04-11 22:39:10 -0400

    updated fetch data from server

---
+++ client/build/precache-manifest.fc2decfd764cf03d95f8f298b180f05b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613c0e54f17fda350c6a5813e658e0c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9df7a85bb89bbe6b183",
+    "url": "/static/css/main.f0e37fe6.chunk.css"
+  },
+  {
+    "revision": "3cdc39a7be4cedabbeb7",
+    "url": "/static/js/2.80b6616e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.80b6616e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9df7a85bb89bbe6b183",
+    "url": "/static/js/main.da338a6e.chunk.js"
+  },
+  {
+    "revision": "228fa098814da32aa5c2",
+    "url": "/static/js/runtime-main.e1daef6b.js"
+  },
+  {
+    "revision": "56ac30abd7e582b10b42c1d96f4a4c89",
+    "url": "/static/media/background.56ac30ab.jpg"
+  }
+]);
--- client/src/components/Favorites.js
+++ client/src/components/Favorites.js
@@ -4,33 +4,43 @@ import React, {useState, useEffect} from 'react'
 function Favorites() {
     const [books, setBooks] = useState([] )
     async function loadFavorites(){
-        const savedBooks = await fetch('/api/favorites/list').then( result => result.json() )
-        setBooks( savedBooks )
+        const savedBooks = await fetch('/api/favorites/list').then( result => result.json() )  
+        let newBookList = savedBooks.dbfetchBooks
+        setBooks( newBookList )
+        console.log('saved books from db are', savedBooks.dbfetchBooks)
+        
     }
+    console.log(books);
 
     useEffect( function(){
         loadFavorites();
     }, [] );
 
+    function handleDeleteBook(id){
+        console.log(id);
+
+    }
+ 
+
     return (
-    <div>
-
-        { books.map( item => 
-            (<div class="row">
-                <div class="col-md-4 d-flex flex-row justify-content-around">
-                    <div class="card mb-4 box-shadow">
-                        <img class="card-img-top" data="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card cap"/>
-                            <div class="card-body">
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    
-                            </div>
+        <>
+             {(books.map( item => 
+                
+                    <div class="col-md-4 d-inline-flex flex-row justify-content-around">
+                        <div class="card mb-4 box-shadow">
+                            <img class="img-thumbnail" src={item.image} alt="Card cap"/>
+                                <h5 class="card-title">{item.title}</h5>
+                                <div class="card-body justify-content-center">
+                                    {/* <p class="card-text">{item.title} </p> */}
+                                    <button onClick={() => handleDeleteBook(item._id)}>Delete</button>
+                                </div>
+                        </div>
                     </div>
-                </div>
-            </div>    
-        ))}
-    )
-    </div>
-    )
+               
+            ))}
+        )
+        </>
+        )
 
-}
+    }
 export default Favorites
